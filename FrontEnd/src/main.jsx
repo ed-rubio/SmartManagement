a8f8ef57bca41bb68c16bdc66496583f2914710f
gagor0202 <gagor0202@gmail.com>
2025-04-06 01:11:37 -0700

    ...

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
+import './CSS/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import Home from './routes/pages/Home.jsx'
+import EoqPage from './routes/pages/EoqPage.jsx'
+import NotFound from './routes/NotFound/NotFound.jsx'
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/home',
+        element: <Home />
+      },
+      {
+        path: '/eogpage',
+        element: <EoqPage />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
---
+++ FrontEnd/src/routes/pages/EoqPage.jsx
@@ -0,0 +1,10 @@
+const EoqPage = () => {
+  return (
+    <div>
+      <h1>EOQ Page</h1>
+      <p>This is the EOQ page.</p>
+    </div>
+  );
+}
+
+export default EoqPage
