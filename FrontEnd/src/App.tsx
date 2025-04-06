e06816f1951149b9abe8a20406d348fb184222f4
gagor0202 <gagor0202@gmail.com>
2025-04-05 21:08:05 -0700

    primer version, sin tailwind

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,27 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from '../'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1> Hola  tonotos</h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='items-center justify-center'>
+      <section className='bg-blue-950 p-4 shadow-md w-full'>
+      <h1 className='text-3xl text-white font-bold text-center'>
+        Jeje Hola
+      </h1>
+      </section>
+      <div className='w-full items-center justify-center'>
+        <button onClick={() => alert('Edita esto o eres puto')} className='bg-blue-500 text-white p-2 mt-4'>
+          CLick me
         </button>
-        <p>
-          Edita <code>src/Tumama.jxs</code> y guarda para no reprobar
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click aqui si eres gei
-      </p>
+    </div>
     </>
   )
 }
--- FrontEnd/src/routes/pages/Home.jsx
+++ FrontEnd/src/routes/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
+
 const Home = () => {
-    return(
-        <>
-        </>
+    return (
     )
 }
 
-export default Home
+export default Home()
