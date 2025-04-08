cdf05031c2d82bf54e523cb51e0223181d16d516
gagor0202 <gagor0202@gmail.com>
2025-04-08 16:01:28 -0700

    hola

--- FrontEnd/src/components/home_components/TopTittle.jsx
+++ FrontEnd/src/components/home_components/TopTittle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TopTittle = () => {
     return (
-        <section className="w-full flex top-0 h-fit bg-Navbar text-center py-16">
+        <section className="w-full flex top-0 h-fit bg-Navbar text-center py-4 rounded-2xl">
             <div className="container mx-auto px-4">
                 <h1 className="text-5xl font-extrabold text-Base mb-4">
                     Bienvenidos a SmartManagement
